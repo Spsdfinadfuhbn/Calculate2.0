a7a7e78bfb617842b85ad5f3efc68b70e2ccdd9a
Nu-dopustim-Nik <26246809-Nu-dopustim-Nik@users.noreply.replit.com>
2023-09-20 15:41:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+b=int(input("Введи число:"))
+c=int(input("Введи второе число:"))
+a=input("Введи знак:")
+if a=="*":
+  print(c*b)
+elif a=="-":
+  if c-b>0:
+    print(-(c-b))
+  else:
+    prinbt(c-b)
+elif a=="+":
+  print(c+b)
+elif a=="/":
+   print(c/b)
+try:
+  print(c/b)
+except ZeroDivisionError:
+  print("я так не умею))")
+  
+else:
+    "break"
+
